d3adbe1e4ca1be6761b8fbff350ba7d755518838
Eduardo Mora <35412372+jemorape@users.noreply.github.com>
2025-08-29 23:22:12 +0000

    change config

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,10 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("Login User", async ({ page, context }) => {
+  await context.setDefaultNavigationTimeout(60000); // optional: set a longer navigation timeout
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.goto("https://automationexercise.com", {
+    waitUntil: "domcontentloaded", // ensure the DOM is fully loaded
+  });
 });
